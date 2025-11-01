c2562f698c02d5a88970fc8990322adf92879a5f
Vijay Karena <vijaykarena360@gmail.com>
2025-11-02 01:07:55 +0530

    create index route

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -14,6 +19,12 @@ async function main() {
 
 app.get("/", (req, res) => {
   res.send("Hi, i am root");
+});
+
+// Index Route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
 
 app.listen(8080, () => {
