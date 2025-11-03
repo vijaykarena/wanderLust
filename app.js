fe372f82cef000126bf19f12e4a7e676aea3a83a
Vijay Karena <vijaykarena360@gmail.com>
2025-11-03 11:26:08 +0530

    create new - page & route

--- app.js
+++ app.js
@@ -29,11 +29,24 @@ app.get("/listings", async (req, res) => {
   res.render("listings/index.ejs", { allListings });
 });
 
+// New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
 // Show Route
 app.get("/listings/:id", async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/show.ejs", { listing });
+});
+
+// Create Route
+app.post("/listings", async (req, res) => {
+  // let {title, description, image, price, country, location } = req.body;
+  let newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
 });
 
 app.listen(8080, () => {
