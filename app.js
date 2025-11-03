7f14a429fcc5e27b1abe0ff7f0484de2a181b9aa
Vijay Karena <vijaykarena360@gmail.com>
2025-11-03 12:02:42 +0530

    edit & update route

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(methodOverride("_method"));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -47,6 +50,20 @@ app.post("/listings", async (req, res) => {
   let newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings");
+});
+
+// Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+// Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
 });
 
 app.listen(8080, () => {
