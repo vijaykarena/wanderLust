b42ce9a351554981652a7c020e4a1cdfe17c205d
Vijay Karena <vijaykarena360@gmail.com>
2025-11-05 10:46:13 +0530

    ejs-mate, navbar, footer

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.engine("ejs", ejsMate);
+
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(express.urlencoded({ extended: true }));
 
