77b8d0dc10100d59e58229286878fcd37e27c0b6
Vijay Karena <vijaykarena360@gmail.com>
2025-11-22 23:21:40 +0530

    error page

--- app.js
+++ app.js
@@ -111,7 +111,8 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong!" } = err;
-  res.status(statusCode).send(message);
+  res.status(statusCode).render("error.ejs", { message });
+  // res.status(statusCode).send(message);
 });
 
 app.listen(8080, () => {
