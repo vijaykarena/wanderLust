914b56801cd0140cd2979f3514dd160f43e6eb82
Vijay Karena <vijaykarena360@gmail.com>
2025-11-30 16:54:53 +0530

    session & session expire logic

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+
+const sessionOptions = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
+
+app.use(session(sessionOptions));
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
