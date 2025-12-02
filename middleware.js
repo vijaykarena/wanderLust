58199ebce0b3aea843f09eeef3aab88148e8cf40
Vijay Karena <vijaykarena360@gmail.com>
2025-12-02 13:23:40 +0530

    post-login page

--- middleware.js
+++ middleware.js
@@ -1,7 +1,16 @@
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
+    // after login, redirect user to path that they trying to access
+    req.session.redirectUrl = req.originalUrl;
     req.flash("error", "you must be logged in to create listing!");
     return res.redirect("/login");
+  }
+  next();
+};
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+  if (req.session.redirectUrl) {
+    res.locals.redirectUrl = req.session.redirectUrl;
   }
   next();
 };
