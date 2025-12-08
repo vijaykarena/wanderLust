d43afe9ab44b29dbd898dbd60e82e3957b5de22c
Vijay Karena <vijaykarena360@gmail.com>
2025-12-08 18:58:52 +0530

    coordinates to exact location on map

--- public/js/map.js
+++ public/js/map.js
@@ -1,16 +1,26 @@
+console.log(listing.geometry.coordinates);
 
-    // Initialize map
-    const map = L.map("map").setView([21.1702, 72.8311], 13);
+// Initialize map
+let coordinates = listing.geometry.coordinates;
+const map = L.map("map").setView([coordinates[1], coordinates[0]], 13); // [lat, lon]
 
-    // Add tile layer (required to see map tiles)
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 19,
-      attribution: "&copy; OpenStreetMap contributors",
-    }).addTo(map);
+// Add tile layer (required to see map tiles)
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: "&copy; OpenStreetMap contributors",
+}).addTo(map);
 
-    // Add a marker
-    L.marker([21.1702, 72.8311])
-      .addTo(map)
-      .bindPopup("Hello from Leaflet!")
-      .openPopup();
- 
+// custom icon
+const compassIcon = L.divIcon({
+  html: `<i class="fa-regular fa-compass"></i>`,
+  className: "custom-marker",
+  iconSize: [30, 30],
+  iconAnchor: [15, 30], // ⬅ tells Leaflet where the "point" is
+  popupAnchor: [5, -28], // ⬅ move popup above the icon
+});
+
+// Add a marker
+L.marker([coordinates[1], coordinates[0]], { icon: compassIcon }) // Listing.geometry.coordinates // [lat, lon]
+  .addTo(map)
+  .bindPopup(listing.title)
+  .openPopup();
